ca0e82e7f58c49743ea251161c8e33a7b1d0b9ee
vinnireddy <58021550+vinnireddy@users.noreply.github.com>
2019-11-21 11:15:06 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        public void test1(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine("a is bigger");
+            }
+            else
+            {
+                Console.WriteLine("b is bigger");
+            }
+
+        }
+    }
+    class mainprg
+    {
+        static void Main()
+        {
+            Class2 obj = new Class2();
+            obj.test1(10, 11);
+
+        }
+    }
+    
+    }
+
