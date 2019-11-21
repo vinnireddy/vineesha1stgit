5c340e1245acd4d760e205a7d4255d787537d3ee
vinnireddy <58021550+vinnireddy@users.noreply.github.com>
2019-11-21 11:07:55 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   
+
+    class pr
+    {
+        static void test(int r)
+        {
+            double a = (3.14) * r * r;
+            Console.WriteLine("area of circle :" + a);
+        }
+        static void Main(string [] vinni)
+        {
+            
+             test(4);
+        }
+    }
+}
